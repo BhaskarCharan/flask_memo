11093096f88364cc32f8ded215a41d6d71806775
BhaskarCharan <76473485+BhaskarCharan@users.noreply.github.com>
2021-03-07 19:37:06 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flaskblog import create_app
 
 app = create_app()
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yctcfllhxlheos:8d68c07a4f5eb23a6439678b3d8dfaa477c66f58e7c6f2998e0880520a639b0e@ec2-54-163-140-104.compute-1.amazonaws.com:5432/deaehc7vkquo3l'
 if __name__ == '__main__':
     app.run(debug=True)
--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = ('postgres://mjigjcdxhoacny:326f53fd1227b9048314cce344089aaccfbe74e516eef71efbee19d970a6257c@ec2-54-197-228-62.compute-1.amazonaws.com:5432/df43bb7tk1sdos')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
