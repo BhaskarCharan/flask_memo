1329437a9672a506f8753d8a9acb8f6187d5e291
BhaskarCharan <76473485+BhaskarCharan@users.noreply.github.com>
2021-03-07 20:48:05 +0530

    initial commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,8 +4,7 @@ import psycopg2
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    SQLALCHEMY_DATABASE_URI = psycopg2.connect('postgres://yctcfllhxlheos:8d68c07a4f5eb23a6439678b3d8dfaa477c66f58e7c6f2998e0880520a639b0e@ec2-54-163-140-104.compute-1.amazonaws.com:5432/deaehc7vkquo3l', sslmode='require')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
