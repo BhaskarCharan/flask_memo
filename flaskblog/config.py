8f206fb003027330d7a9cd194b670e6188e1e6cb
BhaskarCharan <76473485+BhaskarCharan@users.noreply.github.com>
2021-03-07 20:25:29 +0530

    initial commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,11 @@
 import os
+import psycopg2
 
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
